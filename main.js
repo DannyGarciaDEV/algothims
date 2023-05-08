dcea38356fcf0a08f9dfe7a9a1ccb6933ec0e13d
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-05-07 22:34:34 -0400

    fix

--- main.js
+++ main.js
@@ -60,4 +60,37 @@ class Arr {
       const arr = new Arr(1, 2, 3, 4, 5);
       assert.deepEqual(arr.reverse(), [5, 4, 3, 2, 1]);
     });
-  })  
+  
+    it("Should implement slice", () => {
+        const arr = new Arr(1, 2, 3, 4, 5);
+        assert.deepEqual(arr.slice(), [1, 2, 3, 4, 5]);
+        assert.deepEqual(arr.slice(1), [2, 3, 4, 5]);
+        assert.deepEqual(arr.slice(2, 4), [3, 4]);
+      });
+    
+      it("Should implement splice", () => {
+        const arr = new Arr(1, 2, 3, 4, 5);
+        assert.deepEqual(arr.splice(2, 0, 6), []);
+        assert.deepEqual(arr.arr, [1, 2, 6, 3, 4, 5]);
+        assert.deepEqual(arr.splice(3, 1), [3]);
+        assert.deepEqual(arr.arr, [1, 2, 6, 4, 5]);
+        assert.deepEqual(arr.splice(2, 1, 3), [6]);
+        assert.deepEqual(arr.arr, [1, 2, 3, 4, 5]);
+        assert.deepEqual(arr.splice(0, 2, 6, 7, 8), [1, 2]);
+        assert.deepEqual(arr.arr, [6, 7, 8, 3, 4, 5]);
+        assert.deepEqual(arr.splice(arr.arr.length - 3, 2), [3, 4]);
+        assert.deepEqual(arr.arr, [6, 7, 8, 5]);
+        assert.deepEqual(arr.splice(-2, 1), [8]);
+        assert.deepEqual(arr.arr, [6, 7, 5]);
+        assert.deepEqual(arr.splice(1), [7, 5]);
+        assert.deepEqual(arr.arr, [6]);
+      });
+    });
+    
+    xdescribe("Strings", () => {
+      it("Should implement split", () => {})
+      
+      it("Should implement replace", () => {})
+    });
+    
+    mocha.run();  
